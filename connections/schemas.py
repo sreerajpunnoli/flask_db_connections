4e05a6dea97276be833c9c15c62e1a69dbc48f05
sreerajpunnoli <43034292+sreerajpunnoli@users.noreply.github.com>
2021-04-14 13:54:29 -0300

    Get Connection Tests added
    Added get connection test
    Added duplication check and test

--- connections/schemas.py
+++ connections/schemas.py
@@ -25,34 +25,37 @@ class PersonSchema(BaseModelSchema):
     class Meta:
         model = Person
 
-# Custom validator
-def must_not_be_blank(data):
-    if not data:
-        raise exceptions.ValidationError("Data not provided.")
-
 
 class ConnectionSchema(BaseModelSchema):
     from_person_id = fields.Integer()
     to_person_id = fields.Integer()
     connection_type = EnumField(ConnectionType)
-    from_person = fields.Nested(PersonSchema, validate=must_not_be_blank)
-    to_person = fields.Nested(PersonSchema, validate=must_not_be_blank)
+    from_person = fields.Nested(PersonSchema, dump_only=True)
+    to_person = fields.Nested(PersonSchema, dump_only=True)
     
     @validates_schema
     def validate_connection_type(self, connection):
         if connection['connection_type'] in [ConnectionType.son, ConnectionType.daughter]:
             from_person = Person.query.get(connection['from_person_id'])
             to_person = Person.query.get(connection['to_person_id'])
-            if from_person.date_of_birth < to_person.date_of_birth:
+            if (from_person and to_person) and from_person.date_of_birth < to_person.date_of_birth:
                 raise exceptions.ValidationError("Invalid connection - {} older than parent."\
                                       .format(connection['connection_type'].value))
         elif connection['connection_type'] in [ConnectionType.father, ConnectionType.mother]:
             from_person = Person.query.get(connection['from_person_id'])
             to_person = Person.query.get(connection['to_person_id'])
-            if from_person.date_of_birth > to_person.date_of_birth:
+            if (from_person and to_person) and from_person.date_of_birth > to_person.date_of_birth:
                 raise exceptions.ValidationError("Invalid connection - {} younger than child."\
                                       .format(connection['connection_type'].value))
 
+    @validates_schema
+    def validate_connection_duplication(self, connection):
+        existing_connection = Connection.query.filter_by(from_person_id=connection['from_person_id'], \
+                                                         to_person_id=connection['to_person_id'], \
+                                                         connection_type=connection['connection_type']).first()
+        if existing_connection:
+            raise exceptions.ValidationError("Connection already exist.")
+        
     class Meta:
         model = Connection
 
--- tests/functional/connections/test_create_connection.py
+++ tests/functional/connections/test_create_connection.py
@@ -1,7 +1,6 @@
 from datetime import date
 from http import HTTPStatus
 
-import pytest
 from tests.factories import PersonFactory
 
 from connections.models.connection import Connection
@@ -52,3 +51,44 @@ def test_create_connection_parent_and_child_validation(db, testapp):
     assert connection.from_person_id == child.id
     assert connection.to_person_id == parent.id
     assert connection.connection_type.value == 'son'
+
+
+def test_create_connection_duplicate_validation(db, testapp):
+    parent = PersonFactory(date_of_birth=date(1950, 10, 1))
+    child = PersonFactory(date_of_birth=date(1990, 10, 1))
+    db.session.commit()
+    # Child is son to parent
+    payload = {
+        'from_person_id': child.id,
+        'to_person_id': parent.id,
+        'connection_type': 'son',
+    }
+    res = testapp.post('/connections', json=payload)
+
+    assert res.status_code == HTTPStatus.CREATED
+
+    assert 'id' in res.json
+
+    connection = Connection.query.get(res.json['id'])
+
+    assert connection is not None
+    assert connection.from_person_id == child.id
+    assert connection.to_person_id == parent.id
+    assert connection.connection_type.value == 'son'
+    
+    # Try to create connection with same payload once again
+    res = testapp.post('/connections', json=payload)
+    
+    assert res.status_code == HTTPStatus.BAD_REQUEST
+    
+    assert 'description' in res.json
+    assert 'Input failed validation.' == res.json['description']
+    assert 'errors' in res.json
+    assert '_schema' in res.json['errors']
+    error_messages = res.json['errors']['_schema']
+    assert len(error_messages) == 1
+    assert 'Connection already exist.' == error_messages[0]
+    
+    
+    
+    
