491da4e33e659015d77bf31e92b3854dce5ea1ee
sreerajpunnoli <43034292+sreerajpunnoli@users.noreply.github.com>
2021-04-14 21:24:13 -0300

    Added mutual friends functionality tests

---
+++ tests/functional/people/test_mutual_friends.py
@@ -0,0 +1,66 @@
+from http import HTTPStatus
+
+from tests.factories import PersonFactory, ConnectionFactory
+
+EXPECTED_FIELDS = [
+    'id',
+    'first_name',
+    'last_name',
+    'email',
+]
+
+
+def create_friends():
+    person = PersonFactory()
+    target_person = PersonFactory()
+
+    # some decoy connections (not mutual)
+    ConnectionFactory.create_batch(2, to_person=person)
+    ConnectionFactory.create_batch(2, to_person=target_person)
+
+    mutual_friends = PersonFactory.create_batch(3)
+    for f in mutual_friends:
+        ConnectionFactory(from_person=person, to_person=f, connection_type='friend')
+        ConnectionFactory(from_person=target_person, to_person=f, connection_type='friend')
+
+    # mutual connections, but not friends
+    decoy = PersonFactory()
+    ConnectionFactory(from_person=person, to_person=decoy, connection_type='coworker')
+    ConnectionFactory(from_person=target_person, to_person=decoy, connection_type='coworker')
+    
+    return person, target_person, mutual_friends
+    
+
+def check_mutual_friends(res, mutual_friends):
+    expected_mutual_friend_ids = [f.id for f in mutual_friends]
+    
+    assert res.status_code == HTTPStatus.OK
+
+    assert len(res.json) == 3
+    for f in res.json:
+        for field in EXPECTED_FIELDS:
+            assert field in f
+        assert f['id'] in expected_mutual_friend_ids
+
+
+def test_can_get_mutual_friends(db, testapp):
+    person, target_person, mutual_friends = create_friends()
+    db.session.commit()
+    
+    res = testapp.get(f'/people/{person.id}/mutual_friends?target_id={target_person.id}')
+
+    check_mutual_friends(res, mutual_friends)
+
+
+def test_mutual_frieds_after_swapping(db, testapp):
+    person, target_person, mutual_friends = create_friends()
+    db.session.commit()
+    
+    res = testapp.get(f'/people/{person.id}/mutual_friends?target_id={target_person.id}')
+    # get mutual friends after swapping person and target_person
+    swapped_res = testapp.get(f'/people/{target_person.id}/mutual_friends?target_id={person.id}')
+    
+    check_mutual_friends(res, mutual_friends)
+    check_mutual_friends(swapped_res, mutual_friends)
+    
+    assert res.json == swapped_res.json
